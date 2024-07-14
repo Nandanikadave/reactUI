96225c63bdbfa529aac7a096604900852dd1fd25
NandaniKadave <nandani.kadave@somaiya.edu>
2024-07-14 11:19:22 -0400

    sidebar

--- ui1/src/App.jsx
+++ ui1/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BriefcaseIcon, EnvelopeIcon, CreditCardIcon,BellAlertIcon } from "@heroicons/react/24/outline";
+import { BriefcaseIcon, EnvelopeIcon, CreditCardIcon,BellAlertIcon,TrashIcon, PencilSquareIcon,UsersIcon,UserPlusIcon,ChatBubbleLeftIcon,EyeIcon } from "@heroicons/react/24/outline";
 import { Menu, MenuItem, MenuItems ,MenuButton } from '@headlessui/react';
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import logo from "./images/image.jpg"
@@ -102,19 +102,73 @@ const App = () => {
     <div className="border-b-2 border-gray-200 py-5">
     <div className=" absolute -mt-4 left-20 hidden sm:mb-8 sm:flex ">
     {navigation2.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    aria-current={item.current ? 'page' : undefined}
-                    className={`px-7 py-2 text-sm font-medium flex items-center space-x-2 ${
-                      item.current ? 'text-oranges' : 'text-gray-400 hover:text-oranges hover:text-white hover:underline'
-                    }`}
-                  >
-                     {item.name}
-                  </a>
-                ))}
-                </div>
-                </div>
+  <a
+    key={item.name}
+    href={item.href}
+    aria-current={item.current ? 'page' : undefined}
+    className={`px-7 py-2 text-sm font-medium flex items-center space-x-2 relative ${
+      item.current
+        ? 'text-oranges after:bg-oranges after:scale-x-100'
+        : 'text-gray-400 hover:text-oranges hover:text-white'
+    } after:absolute after:bottom-0 after:left-10 after:h-0.5 after:w-14`}
+  >
+    {item.name}
+  </a>
+))}
+</div>
+  </div>
+  
+  <div className=" absolute right-1 w-72 h-full max-h-full border-l-2 border-gray-200">
+    <div className="flex flex-row space-x-12">
+  <button className="text-sm leading-6 top-5 hover:text-white relative left-7 bg-orange-200 -ring-1 rounded-md h-8 w-28 ring-1 ring-orange-500 hover:bg-oranges text-oranges flex items-center justify-center space-x-2">
+  <TrashIcon className="h-4 w-4 " />
+  <span>Delete Job</span>
+</button>
+
+    <button className="text-sm leading-6 top-5 hover:text-white relative bg-orange-200 -ring-1 rounded-md h-8 w-28 ring-1 ring-orange-500 hover:bg-oranges text-oranges flex items-center justify-center space-x-2">
+    <PencilSquareIcon class="h-4 w-4" />
+    <span>Edit Job</span></button>
+  </div>
+  <div className="pt-12 flex flex-col space-y-2 p-4">
+  <div className="w-full border-b-2 border-gray-100 h-12 flex items-center">
+  <dl className="w-full divide-y divide-gray-100">
+    <div className="flex justify-between items-center px-4 py-6 sm:grid sm:grid-cols-3 sm:px-0 ">
+      <dt className="flex items-center text-sm font-medium leading-6 text-gray-700">
+        <UsersIcon className="h-5 w-5 mr-2" /> Applicant
+      </dt>
+      <dd className="mt-1 text-sm leading-6 text-gray-900 sm:col-span-2 sm:mt-0 sm:ml-32">400</dd>
+    </div>
+  </dl>
+</div>
+
+  <div className="w-full border-b-2 border-gray-100 h-12 flex items-center">
+    <dl className="w-full divide-y divide-gray-100">
+      <div className="flex justify-between items-center px-4 py-6 sm:grid sm:grid-cols-3 sm:px-0">
+        <dt className="flex items-center text-sm font-medium leading-6 text-gray-700"> <UserPlusIcon className="h-5 w-5 mr-2" /> Matches</dt>
+        <dd className="mt-1 text-sm leading-6 text-gray-900 sm:col-span-2 sm:mt-0 sm:ml-32">100</dd>
+      </div>
+    </dl>
+  </div>
+  <div className="w-full border-b-2 border-gray-100 h-12 flex items-center">
+    <dl className="w-full divide-y divide-gray-100">
+      <div className="flex justify-between items-center px-4 py-6 sm:grid sm:grid-cols-3 sm:px-0">
+        <dt className="flex items-center text-sm font-medium leading-6 text-gray-700"><ChatBubbleLeftIcon className="h-5 w-5 mr-2" />Messages</dt>
+        <dd className="mt-1 text-sm leading-6 text-gray-900 sm:col-span-2 sm:mt-0  sm:ml-32">147</dd>
+      </div>
+    </dl>
+  </div>
+  <div className="w-full h-12 flex items-center">
+    <dl className="w-full divide-y divide-gray-100">
+      <div className="flex justify-between items-center px-4 py-6 sm:grid sm:grid-cols-3 sm:px-0">
+        <dt className="flex items-center text-sm font-medium leading-6 text-gray-700"><EyeIcon className="h-5 w-5 mr-2" />Views</dt>
+        <dd className="mt-1 text-sm leading-6 text-gray-900 sm:col-span-2 sm:mt-0  sm:ml-32">800</dd>
+      </div>
+    </dl>
+  </div>
+  
+</div>
+
+  </div>
     </>
   )
 }
